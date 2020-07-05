2248f5993f25bdc49ebfe4e4ebe760cebc30cbf5
Vox Humana <jonprine@gmail.com>
2020-07-05 12:07:01 -0500

    removed date/hour var def  out of function

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,13 @@
-$("document").ready(function() {
+// variables
+
+var currentDate = moment().format("dddd, MMMM Do");
+var currentTime = moment().format("H");
+
+$("document").ready(function () {
 
     // set current date
 
-    var currentDate = moment().format("dddd, MMMM Do");
-
     $("#currentDay").text(currentDate);
-
-    // set current hour
-    var currentTime = moment().format("H");
-    console.log(currentTime);
 
 
 
